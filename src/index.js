9bd6daead70a113abcb0fb7935a875fb9900d793
Midnight-Time <p.v.filipenko.work@gmail.com>
2023-10-30 00:43:10 +0300

    send form to server logic added

--- src/index.js
+++ src/index.js
@@ -57,6 +57,7 @@ const sendlObj = {
   email: "",
   phone: "",
   message: "",
+  sendTime: new Date(),
 };
 
 const phoneValidation = (input) => {
@@ -133,12 +134,31 @@ const showMessage = function (message) {
   });
 };
 
+const sendData = async function (data) {
+  const response = await fetch(
+    "https://my-project-83d90-default-rtdb.firebaseio.com/portfolioForm.json",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error(`Something went wrong! Error:${response.status}`);
+  } else {
+    showMessage("Your message was successfully delivered!");
+  }
+};
+
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   if (sendlObj) {
     showMessage("Your message was successfully delivered!");
   }
-  // sendData(sendlObj).catch((err) => showMessage(err.message));
+  sendData(sendlObj).catch((err) => showMessage(err.message));
 
   // Clear the inputs
   allInputs.forEach((input) => {
